ace5547baf744cd80ff10a66cb4e43dad10201a0
igorpuszman <igibabes@gmail.com>
2020-04-24 15:25:22 +0200

    zadanie

---
+++ zadplik.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+	double t[521];
+	string wynik;
+	for (int i=521; i>=0; i--){
+     t[i]=i*2;
+	
+	}
+	for (int i=521;i>=0;i--)	
+	cout<<t[i]<<" ";	
+	return 0;
+
+}
